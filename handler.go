3f2bb7446060a12302cebdb5300484167192ebc3
Invictus321 <davidwales2@gmail.com>
2017-05-10 11:31:42 +1000

    Add bit to add up each month

--- handler.go
+++ handler.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type handler struct {
@@ -12,7 +14,30 @@ type handler struct {
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		w.WriteHeader(http.StatusNotImplemented)
+		month := r.FormValue("month")
+		if month == "" {
+			month = time.Now().Format("01")
+		}
+		year := r.FormValue("year")
+		if year == "" {
+			year = time.Now().Format("2006")
+		}
+		monthInt, err := strconv.ParseInt(month, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		yearInt, err := strconv.ParseInt(year, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		amount, err := h.writer.AddThisMonth(monthInt, yearInt)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(amount)
 	case "POST":
 		amount, err := strconv.ParseInt(r.FormValue("amount"), 10, 64)
 		if err != nil {
--- writer.go
+++ writer.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type Writer interface {
+	AddThisMonth(month, year int64) (float32, error)
 	WriteAmount(amount int64) error
 }
 
@@ -15,6 +19,30 @@ type writer struct {
 }
 
 var dateFormat = "02/01/2006"
+
+func (w writer) AddThisMonth(month, year int64) (float32, error) {
+	monthString := fmt.Sprintf("%d/%d", month, year)
+	f, err := os.Open(w.filename)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	amount := float32(0.0)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), ",")
+		if strings.Contains(fields[0], monthString) {
+			amountToAdd, err := strconv.ParseFloat(fields[1], 32)
+			if err != nil {
+				return 0, err
+			}
+			amount = amount + float32(amountToAdd)
+		}
+	}
+	return amount, nil
+}
 
 func (w writer) WriteAmount(amount int64) error {
 	f, err := os.OpenFile(w.filename, os.O_APPEND|os.O_WRONLY, 0666)
