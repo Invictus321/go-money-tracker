9175e35c00ba30b5d94fec5df8e65bd33fa07654
Invictus321 <davidwales2@gmail.com>
2017-05-10 11:09:38 +1000

    Stuff

--- handler.go
+++ handler.go
@@ -10,20 +10,27 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	amount, err := strconv.ParseInt(r.FormValue("amount"), 10, 64)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusNotImplemented)
+	case "POST":
+		amount, err := strconv.ParseInt(r.FormValue("amount"), 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if amount > 10000 {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		if err := h.writer.WriteAmount(amount); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
-	if amount > 10000 {
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-	if err := h.writer.WriteAmount(amount); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
 }
 
 func HandlerFromEnv() handler {
